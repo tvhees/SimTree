d884e3c30a11d791c1208d7e37fbec3c32d32281
tvhees <t.vanheeswijck@gmail.com>
2021-07-17 22:10:53 +0930

    Initial refactor - simplify/extract existing code in place

--- Assets/_scripts/TreeManager.cs
+++ Assets/_scripts/TreeManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System;
+using System.Linq;
 
 public class TreeManager : MonoBehaviour
 {
@@ -22,52 +23,60 @@ public class TreeManager : MonoBehaviour
         branch.GetComponent<TreeTile>().tileRenderer.sortingLayerName = "Tree";
     }
 
+    void DestroyTile(GameObject branch)
+    {
+        PlayerManager.Instance.activeTiles.Remove(branch);
+        Destroy(branch);
+    }
+
     void AddToTreeParentAndLayer(GameObject branch)
     {
+        // TODO: Fix bug where Wildfire destroyed tiles no longer exist here
         branch.transform.SetParent(transform);
         branch.layer = LayerMask.NameToLayer("TreeTiles");
+    }
+
+    bool IsConnectedDownwards(GameObject branch)
+    {
+        return branch.GetComponent<TreeTile>().directionsDown.Any(d => d);
+    }
+
+    void ActivateAndHighlightBranch(GameObject branch)
+    {
+        branch.transform.SetParent(transform);
+        branch.layer = LayerMask.NameToLayer("ActiveTiles");
+        GameObject highlight = Instantiate(highlightRing) as GameObject;
+        highlight.transform.position = branch.transform.position - new Vector3(0.0f, 0.0f, 1.0f);
+        highlight.transform.SetParent(branch.transform);
+    }
+
+    void CreateSeasonHolder()
+    {
+        GameObject instance = Instantiate(seasonHolder);
+        instance.name = "CurrentSeason";
+        instance.GetComponent<SeasonController>().AddTiles(PlayerManager.Instance.activeTiles, PlayerManager.Instance.seasonTiles, 2);
     }
 
     public void ChangeSeason()
     {
         PlayerManager.Instance.seasonTiles.ForEach(AddToActiveTiles);
-
         PlayerManager.Instance.treeTiles.ForEach(AddToTreeParentAndLayer);
 
         // Create a temporary list of tiles that need to be destroyed later
         List<GameObject> destroyList = new List<GameObject>();
 
-        foreach (GameObject branch in PlayerManager.Instance.activeTiles)
-        {
-            bool[] directionsDown = branch.GetComponent<TreeTile>().directionsDown;
-            if (directionsDown[0] || directionsDown[1] || directionsDown[2])
-            {
-                branch.transform.SetParent(transform);
-                branch.layer = LayerMask.NameToLayer("ActiveTiles");
-                GameObject highlight = Instantiate(highlightRing) as GameObject;
-                highlight.transform.position = branch.transform.position - new Vector3(0.0f, 0.0f, 1.0f);
-                highlight.transform.SetParent(branch.transform);
-            }
-            else
-            {
-                // Add an active tile that has no roots to the destroy list
-                destroyList.Add(branch);
-            }
-        }
+        PlayerManager.Instance.activeTiles
+            .Where(IsConnectedDownwards).ToList()
+            .ForEach(ActivateAndHighlightBranch);
 
-        // Destroy tiles after removing them from the active list
-        foreach (GameObject branch in destroyList)
-        {
-            PlayerManager.Instance.activeTiles.Remove(branch);
-            Destroy(branch);
-        }
+        var nextTiles = PlayerManager.Instance.activeTiles
+            .ToLookup(IsConnectedDownwards);
+
+        nextTiles[true].ToList().ForEach(ActivateAndHighlightBranch);
+        nextTiles[false].ToList().ForEach(DestroyTile);
 
         // Move the whole tree downwards, add new season tiles
         transform.Translate(new Vector3(0.0f, -Mathf.Sqrt(3) * hexSize, 0.0f));
-        PlayerManager.Instance.seasonTiles.Clear();
         PlayerManager.Instance.ChangeSeason();
-        GameObject instance = Instantiate(seasonHolder);
-        instance.name = "CurrentSeason";
-        instance.GetComponent<SeasonController>().AddTiles(PlayerManager.Instance.activeTiles, PlayerManager.Instance.seasonTiles, 2);
     }
 }
