00807e8ffe67da2a3ecaea2c79dfee5ef8f0490f
tvhees <t.vanheeswijck@gmail.com>
2021-07-17 18:22:16 +0930

    More auto changes

--- Assets/_scripts/TreeManager.cs
+++ Assets/_scripts/TreeManager.cs
@@ -1,57 +1,73 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 
-public class TreeManager : MonoBehaviour {
+public class TreeManager : MonoBehaviour
+{
 
-	public GameObject treeRoot;
-	public GameObject seasonHolder;
-	public GameObject highlightRing;
-	public float hexSize = 2;
+    public GameObject treeRoot;
+    public GameObject seasonHolder;
+    public GameObject highlightRing;
+    public float hexSize = 2;
 
-	public void NewTree(){
-		Instantiate (treeRoot);
-	}
+    public void NewTree()
+    {
+        Instantiate(treeRoot);
+    }
 
-	public void ChangeSeason(){
-		foreach (GameObject branch in PlayerManager.Instance.seasonTiles) {
-			PlayerManager.Instance.activeTiles.Add (branch);
-			branch.GetComponent<TreeTile> ().tileRenderer.sortingLayerName = "Tree";
-		}
-		foreach (GameObject branch in PlayerManager.Instance.treeTiles) {
-			branch.transform.SetParent (transform);
-			branch.layer = LayerMask.NameToLayer("TreeTiles");
-		}
+    void AddToActiveTiles(GameObject branch)
+    {
+        PlayerManager.Instance.activeTiles.Add(branch);
+        branch.GetComponent<TreeTile>().tileRenderer.sortingLayerName = "Tree";
+    }
 
-		// Create a temporary list of tiles that need to be destroyed later
-		List<GameObject> destroyList = new List<GameObject>();
+    void AddToTreeParentAndLayer(GameObject branch)
+    {
+        branch.transform.SetParent(transform);
+        branch.layer = LayerMask.NameToLayer("TreeTiles");
+    }
 
-		foreach (GameObject branch in PlayerManager.Instance.activeTiles) {
-			bool[] directionsDown = branch.GetComponent<TreeTile> ().directionsDown;
-			if (directionsDown [0] || directionsDown [1] || directionsDown [2]) {
-				branch.transform.SetParent (transform);
-				branch.layer = LayerMask.NameToLayer ("ActiveTiles");
-				GameObject highlight = Instantiate (highlightRing) as GameObject;
-				highlight.transform.position = branch.transform.position - new Vector3 (0.0f, 0.0f, 1.0f);
-				highlight.transform.SetParent (branch.transform);
-			} else {
-				// Add an active tile that has no roots to the destroy list
-				destroyList.Add (branch);
-			}
-		}
+    public void ChangeSeason()
+    {
+        PlayerManager.Instance.seasonTiles.ForEach(AddToActiveTiles);
 
-		// Destroy tiles after removing them from the active list
-		foreach (GameObject branch in destroyList){
-			PlayerManager.Instance.activeTiles.Remove (branch);
-			Destroy (branch);
-		}
+        PlayerManager.Instance.treeTiles.ForEach(AddToTreeParentAndLayer);
 
-		// Move the whole tree downwards, add new season tiles
-		transform.Translate (new Vector3 (0.0f, -Mathf.Sqrt(3) * hexSize, 0.0f));
-		PlayerManager.Instance.seasonTiles.Clear ();
-		PlayerManager.Instance.ChangeSeason ();
-		GameObject instance = Instantiate (seasonHolder);
-		instance.name = "CurrentSeason";
-		instance.GetComponent<SeasonController> ().AddTiles (PlayerManager.Instance.activeTiles, PlayerManager.Instance.seasonTiles, 2);
-	}
+        // Create a temporary list of tiles that need to be destroyed later
+        List<GameObject> destroyList = new List<GameObject>();
+
+        foreach (GameObject branch in PlayerManager.Instance.activeTiles)
+        {
+            bool[] directionsDown = branch.GetComponent<TreeTile>().directionsDown;
+            if (directionsDown[0] || directionsDown[1] || directionsDown[2])
+            {
+                branch.transform.SetParent(transform);
+                branch.layer = LayerMask.NameToLayer("ActiveTiles");
+                GameObject highlight = Instantiate(highlightRing) as GameObject;
+                highlight.transform.position = branch.transform.position - new Vector3(0.0f, 0.0f, 1.0f);
+                highlight.transform.SetParent(branch.transform);
+            }
+            else
+            {
+                // Add an active tile that has no roots to the destroy list
+                destroyList.Add(branch);
+            }
+        }
+
+        // Destroy tiles after removing them from the active list
+        foreach (GameObject branch in destroyList)
+        {
+            PlayerManager.Instance.activeTiles.Remove(branch);
+            Destroy(branch);
+        }
+
+        // Move the whole tree downwards, add new season tiles
+        transform.Translate(new Vector3(0.0f, -Mathf.Sqrt(3) * hexSize, 0.0f));
+        PlayerManager.Instance.seasonTiles.Clear();
+        PlayerManager.Instance.ChangeSeason();
+        GameObject instance = Instantiate(seasonHolder);
+        instance.name = "CurrentSeason";
+        instance.GetComponent<SeasonController>().AddTiles(PlayerManager.Instance.activeTiles, PlayerManager.Instance.seasonTiles, 2);
+    }
 }
--- Assets/_scripts/WeatherController.cs
+++ Assets/_scripts/WeatherController.cs
@@ -1,90 +1,102 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class WeatherController : MonoBehaviour {
+public class WeatherController : MonoBehaviour
+{
 
-	public GameObject windZone;
-	public GameObject rainParticles;
-	public GameObject snowParticles;
-	public GameObject windParticles;
+    public GameObject windZone;
+    public GameObject rainParticles;
+    public GameObject snowParticles;
+    public GameObject windParticles;
 
-	private bool rainStop;
-	private bool snowStop;
-	private bool windStop;
+    private bool rainStop;
+    private bool snowStop;
+    private bool windStop;
 
-	public void Rain(){
+    public void Rain()
+    {
 
-		PlayerManager.Instance.water += 4;
+        PlayerManager.Instance.water += 4;
 
-		ParticleSystem rainSys = rainParticles.GetComponent<ParticleSystem> ();
-		if (rainStop) {
-			// need to stop any other weather systems
-			Fair ();
+        ParticleSystem rainSys = rainParticles.GetComponent<ParticleSystem>();
+        if (rainStop)
+        {
+            // need to stop any other weather systems
+            Fair();
 
-			var vel = rainSys.velocityOverLifetime;
+            var vel = rainSys.velocityOverLifetime;
 
-			AnimationCurve curve = new AnimationCurve();
-			curve.AddKey( 0.0f, 1.0f );
-			curve.AddKey( 1.0f, 1.0f );
-			vel.x = new ParticleSystem.MinMaxCurve(Random.Range (-15.0f, 15.0f), curve);
-			rainSys.Play ();
-			rainStop = false;
-		}
-	}
+            AnimationCurve curve = new AnimationCurve();
+            curve.AddKey(0.0f, 1.0f);
+            curve.AddKey(1.0f, 1.0f);
+            vel.x = new ParticleSystem.MinMaxCurve(Random.Range(-15.0f, 15.0f), curve);
+            rainSys.Play();
+            rainStop = false;
+        }
+    }
 
-	public void Fair(){
-		rainParticles.GetComponent<ParticleSystem> ().Stop ();
-		rainStop = true;
+    public void Fair()
+    {
+        rainParticles.GetComponent<ParticleSystem>().Stop();
+        rainStop = true;
 
-		snowParticles.GetComponent<ParticleSystem> ().Stop ();
-		snowStop = true;
+        snowParticles.GetComponent<ParticleSystem>().Stop();
+        snowStop = true;
 
-		windParticles.GetComponent<ParticleSystem> ().Stop ();
-		windStop = true;
-	}
+        windParticles.GetComponent<ParticleSystem>().Stop();
+        windStop = true;
+    }
 
-	public void Sunshine(){
-		if (PlayerManager.Instance.water > 0) {
-			for (int i = 0; i < 2; i++) {
-				if (PlayerManager.Instance.water > 0) {
-					PlayerManager.Instance.water--;
-					PlayerManager.Instance.energy += 2;
-				} 
-			}
-		}
-		else
-			PlayerManager.Instance.energy--;
+    public void Sunshine()
+    {
+        if (PlayerManager.Instance.water > 0)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                if (PlayerManager.Instance.water > 0)
+                {
+                    PlayerManager.Instance.water--;
+                    PlayerManager.Instance.energy += 2;
+                }
+            }
+        }
+        else
+            PlayerManager.Instance.energy--;
 
-		// no weather system change enabled at the moment
-	}
+        // no weather system change enabled at the moment
+    }
 
-	public void Frost(){
-		PlayerManager.Instance.energy -= 1;
+    public void Frost()
+    {
+        PlayerManager.Instance.energy -= 1;
 
-		ParticleSystem snowSys = snowParticles.GetComponent<ParticleSystem> ();
-		if (snowStop) {
-			// stop any other weather systems
-			Fair ();
+        ParticleSystem snowSys = snowParticles.GetComponent<ParticleSystem>();
+        if (snowStop)
+        {
+            // stop any other weather systems
+            Fair();
 
-			var vel = snowSys.velocityOverLifetime;
+            var vel = snowSys.velocityOverLifetime;
 
-			AnimationCurve curve = new AnimationCurve ();
-			curve.AddKey (0.0f, 1.0f);
-			curve.AddKey (1.0f, 1.0f);
-			vel.x = new ParticleSystem.MinMaxCurve (Random.Range (-8.0f, 8.0f), curve);
+            AnimationCurve curve = new AnimationCurve();
+            curve.AddKey(0.0f, 1.0f);
+            curve.AddKey(1.0f, 1.0f);
+            vel.x = new ParticleSystem.MinMaxCurve(Random.Range(-8.0f, 8.0f), curve);
 
-			snowSys.Play ();
-			snowStop = false;
-		}
-	}
+            snowSys.Play();
+            snowStop = false;
+        }
+    }
 
-	public void Wind(){
+    public void Wind()
+    {
 
-		ParticleSystem windSys = windParticles.GetComponent<ParticleSystem> ();
-		if (windStop) {
-			// need to stop any other weather systems
-			windSys.Play ();
-			windStop = false;
-		}
-	}
+        ParticleSystem windSys = windParticles.GetComponent<ParticleSystem>();
+        if (windStop)
+        {
+            // need to stop any other weather systems
+            windSys.Play();
+            windStop = false;
+        }
+    }
 }
